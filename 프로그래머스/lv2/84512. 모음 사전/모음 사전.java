a9c9f625b0cc9af8564a84f2080ec79f7c796b65
GreenTea <sjohn9227@naver.com>
2023-05-15 14:25:00 +0900

    [level 2] Title: 모음 사전, Time: 22.12 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,25 @@
 import java.util.*;
 
 class Solution {
-    String[] strs = new String[]{"A", "E", "I", "O", "U"};
+    
     List<String> list = new ArrayList<>();
     
     public int solution(String word) {
-        int answer = 0;
         dfs("");
-        Collections.sort(list);
-        return list.indexOf(word)+1;
+        return list.indexOf(word);
  
     }
     
     void dfs(String str) {
 
-        if (str.length() == 5) {
+        if (str.length() > 5) {
             return;
         }
         
-        for (int i=0;i<5;i++) {
-            String newStr = str+strs[i];
-            list.add(newStr);
-            dfs(newStr);
+        list.add(str);
+        
+        for (int i=0; i < 5; i++) {
+            dfs(str + "AEIOU".charAt(i));
         }
         
         
