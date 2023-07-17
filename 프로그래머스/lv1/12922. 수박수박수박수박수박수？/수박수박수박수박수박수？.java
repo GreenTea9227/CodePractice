3430c1376c3ffff620430873aa7c372637c2dc6b
GreenTea <sjohn9227@naver.com>
2023-07-17 11:09:15 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.37 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(int n) {
+        return IntStream.range(0,n).mapToObj(i ->
+                i%2==0 ? "수" : "박"
+            ).collect(Collectors.joining());
+    }
+}
