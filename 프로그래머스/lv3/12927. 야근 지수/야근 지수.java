0d86c6a57585d360d8db2cc3062cee8a933a288c
GreenTea <sjohn9227@naver.com>
2023-07-19 15:52:22 +0900

    [level 3] Title: 야근 지수, Time: 121.03 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] works) {
-        long answer = 0;
        Queue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
         for (int work : works) {
             queue.offer(work);
