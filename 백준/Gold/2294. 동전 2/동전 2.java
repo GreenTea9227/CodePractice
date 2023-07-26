c4cc1290d2aa0bed54e8e3f36c6afb9994e0db95
GreenTea <sjohn9227@naver.com>
2023-07-26 13:47:56 +0900

    [Gold V] Title: 동전 2, Time: 236 ms, Memory: 17968 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,33 +1,29 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final int MAX_NUM = 100000;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] numbers = new int[n];
         int[] dp = new int[k + 1];
 
         for (int i = 0; i < n; i++) {
-            list.add(sc.nextInt());
+            numbers[i] = sc.nextInt();
         }
-        Arrays.fill(dp, 100000);
-        Collections.sort(list);
 
+        Arrays.fill(dp, MAX_NUM);
+        dp[0] = 0;
 
         for (int i = 0; i < n; i++) {
-            int v = list.get(i);
-            int num = 1;
-            for (int j = v; j < dp.length; j += v) {
-                dp[j] = Math.min(dp[j], num++);
-            }
-
-            for (int j = v; j < dp.length; j++) {
-                dp[j] = Math.min(dp[j], dp[j - v]+1);
+            for (int j = numbers[i]; j < dp.length; j++) {
+                dp[j] = Math.min(dp[j], dp[j - numbers[i]] + 1);
             }
         }
         System.out.println(dp[k] == 100000 ? -1 : dp[k]);
