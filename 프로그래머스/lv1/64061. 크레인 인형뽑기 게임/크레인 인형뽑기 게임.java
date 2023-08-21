826b94671c1293a250576f7309f7f09fbddca25c
GreenTea <sjohn9227@naver.com>
2023-08-21 15:12:10 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.77 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -4,24 +4,19 @@ class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
         Stack<Integer> stack = new Stack<>();
-        for (int cx : moves) {
-            cx -= 1;
-            int cy = 0;
-            while (cy < board[cx].length) {
-                int current = board[cy][cx];
-                if (current == 0) {
-                    cy++;
+        for (int i=0;i<moves.length;i++) {
+            int cx = moves[i]-1;
+
+            for (int j=0;j<board[cx].length;j++) {
+                if (board[j][cx] == 0)
                     continue;
-                }
-                   
-                
-                board[cy][cx] = 0;
-                if (!stack.isEmpty() && stack.peek() == current) {
+                if (!stack.isEmpty() && stack.peek() == board[j][cx]) {
                     stack.pop();
                     answer+=2;
-                } else {
-                    stack.push(current);
-                }
+                } else 
+                    stack.push(board[j][cx]);
+                
+                board[j][cx] = 0;
                 break;
             }
             
