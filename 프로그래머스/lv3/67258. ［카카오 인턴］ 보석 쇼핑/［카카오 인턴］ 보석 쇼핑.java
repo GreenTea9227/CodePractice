81144cc11d66fa4fad3633e1d281d14149039cf7
GreenTea <sjohn9227@naver.com>
2023-08-29 16:30:32 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 68.98 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] gems) {
+         int[] answer = new int[2];
+        Set<String> set = new HashSet<>(Arrays.asList(gems));
+        Map<String, Integer> map = new HashMap<>();
+
+        int distance = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int i = 0; i < gems.length; i++) {
+            map.put(gems[i], map.getOrDefault(gems[i], 0) + 1);
+            if (map.size() == set.size()) {
+                while (map.size() == set.size()) {
+                    map.put(gems[start],map.get(gems[start])-1);
+                    if (map.get(gems[start]) == 0) {
+                        map.remove(gems[start]);
+                    }
+                    start++;
+                }
+
+                if (i-start < distance) {
+                    answer[0] = start;
+                    answer[1] = i+1;
+                    distance = i - start;
+                }
+            }
+        }
+        return answer;
+    }
+}
