270ea2b5cc2204877d7dedd2d54e94608d7ba225
GreenTea <sjohn9227@naver.com>
2023-09-09 09:24:25 +0900

    [unrated] Title: 공원 산책, Time: 0.15 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    Map<String,int[]> map;
+    public int[] solution(String[] park, String[] routes) {
+        int[] answer = {};
+        int x = 0;
+        int y = 0;
+        for (int i=0;i<park.length;i++) {
+            for (int j=0;j<park[i].length();j++) {
+                if (park[i].charAt(j)=='S') {
+                    x = j;
+                    y = i;
+                }
+            }
+        }
+        map = new HashMap<>();
+        map.put("E",new int[]{1,0});
+        map.put("N",new int[]{0,-1});
+        map.put("S",new int[]{0,1});
+        map.put("W",new int[]{-1,0});
+        for (String s : routes) {
+            String[] strs = s.split(" ");
+            String dir = strs[0];
+            int move = Integer.parseInt(strs[1]);
+            
+            int[] next = logic(park,dir,move,x,y);
+            x = next[0];
+            y = next[1];
+            
+        }
+        return new int[]{y,x};
+    }
+    
+    private int[] logic(String[] park, String dir,int move, int x, int y) {
+        int[] next = map.get(dir);
+        int cx = x;
+        int cy = y;
+        while (move > 0) {
+            cx += next[0];
+            cy += next[1];
+            if (cx<0 || cx>=park[0].length() || cy<0 || cy>=park.length || park[cy].charAt(cx) == 'X') {
+               
+                return new int[]{x,y};
+            }
+            move--;
+            
+        }
+        return new int[]{cx,cy};
+    }
+}
