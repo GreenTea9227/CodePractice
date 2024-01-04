be175e791118401e8325082228eeca4be09aa070
GreenTea <sjohn9227@naver.com>
2024-01-05 01:46:34 +0900

    [Silver II] Title: 종이의 개수, Time: 1372 ms, Memory: 321912 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.kt
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.kt
@@ -14,32 +14,29 @@ fun main() {
             .toIntArray()
     }
 
-    recur(0, 0, T - 1, T - 1)
+    recur(0, 0, T)
 
     answer.forEach { println(it) }
 }
 
-fun recur(x1: Int, y1: Int, x2: Int, y2: Int) {
-    if (check(x1, y1, x2, y2)) {
-        answer[arr[y1][x1]+1] += 1
-        return
-    }
-
-    val len = (x2 - x1 + 1) / 3
-    for (i in y1..y2 step len) {
-        for (j in x1..x2 step len) {
-            recur(j, i, j + len-1, i + len-1)
+fun recur(x: Int, y: Int, n: Int) {
+    if (check(x, y, n)) {
+        answer[arr[y][x] + 1]++
+    } else {
+        val s = n / 3
+        for (i in 0 until 3) {
+            for (j in 0 until 3) {
+                recur(x + j * s, y + i * s, s)
+            }
         }
     }
-
-
 }
 
-fun check(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
-    val first = arr[y1][x1]
-    for (y in y1..y2) {
-        for (x in x1..x2) {
-            if (arr[y][x] != first)
+fun check(x: Int, y: Int, n: Int): Boolean {
+    val first = arr[y][x]
+    for (i in y until (y + n)) {
+        for (j in x until (x + n)) {
+            if (arr[i][j] != first)
                 return false
         }
     }
