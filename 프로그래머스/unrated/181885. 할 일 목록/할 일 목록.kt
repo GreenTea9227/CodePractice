df235ede9973cf4f0ba14cd1afc289ebb15ed56e
GreenTea <sjohn9227@naver.com>
2024-01-07 23:32:32 +0900

    [level unrated] Title: 할 일 목록, Time: 0.04 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.kt
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.kt
@@ -1,4 +1,5 @@
 class Solution {
     fun solution(todo_list: Array<String>, finished: BooleanArray): Array<String> =
-        todo_list.indices.filter { !finished[it] }.map { todo_list[it] }.toTypedArray()
+    
+        todo_list.filterIndexed { index, s ->  !finished[index]}.toTypedArray()
 }
