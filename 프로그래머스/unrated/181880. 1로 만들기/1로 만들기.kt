f3573d9a1d46986e98458cef53e989ed5acf7d12
GreenTea <sjohn9227@naver.com>
2024-01-10 00:32:24 +0900

    [level unrated] Title: 1로 만들기, Time: 11.00 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.kt
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.kt
@@ -1,16 +1,11 @@
 class Solution {
     fun solution(num_list: IntArray): Int {
-        var answer: Int = 0
-        for (n in num_list) {
-            var temp = n
-            while (temp != 1) {
-                temp = if (temp%2==0)
-                    temp/2
-                else
-                    (temp-1)/2
-                answer++
-            }
-        }
-        return answer
+        return num_list.map {
+            generateSequence(it) {num ->
+                if (num %2 ==0) num/2
+                else (num-1)/2
+            }.takeWhile { num -> num != 1 }.count()
+        }.sum()
     }
+
 }
