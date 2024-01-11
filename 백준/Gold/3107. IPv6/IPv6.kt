fc6f907ed45f743fbe1a363f95f5f5da65574462
GreenTea <sjohn9227@naver.com>
2024-01-11 16:20:14 +0900

    [Gold V] Title: IPv6, Time: 152 ms, Memory: 21188 KB -BaekjoonHub

--- 백준/Gold/3107. IPv6/IPv6.kt
+++ 백준/Gold/3107. IPv6/IPv6.kt
@@ -4,23 +4,18 @@ fun main() {
     if (input.startsWith(":")) {
         input = "0$input"
     }
+    input = input.replace("::", ":check:")
 
-    if (input.contains("::")) {
-        val parts = input.split("::")
-        val count1 = parts[0].count { it == ':' }
-        val count2 = parts[1].count { it == ':' }
-        if (parts.size < 8) {
-            input = "${parts[0]}${":0".repeat(8 - count1 - count2 - 2)}:${parts[1]}"
+    val group = input.split(":").count() - 1
+    input = input.replace(":check", ":0".repeat(8 - group))
+
+    val finalParts = input.split(":")
+        .map { str ->
+        "0".repeat(4 - str.length) + str
         }
-    }
+        .joinToString(":")
 
-    val finalParts = input.split(":").toTypedArray()
-
-    for (i in finalParts.indices) {
-        finalParts[i] = "0".repeat(4 - finalParts[i].length) + finalParts[i]
-    }
-
-    println(finalParts.joinToString(":"))
+    println(finalParts)
 }
 
 
