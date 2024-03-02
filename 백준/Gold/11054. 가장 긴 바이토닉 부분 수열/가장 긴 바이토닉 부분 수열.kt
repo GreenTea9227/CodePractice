f437a96e8ba3d51c42c4b781c4832bb31aa11bb0
GreenTea <sjohn9227@naver.com>
2024-03-02 17:42:21 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 236 ms, Memory: 26880 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.kt
@@ -0,0 +1,37 @@
+
+import kotlin.math.max
+
+fun main() {
+    val N = readLine()!!.toInt()
+
+    val arr = readLine()!!.split(" ").map { it.toInt() }.toIntArray()
+
+    val left = findDp(N, arr)
+    val right = findDp(N, arr.reversedArray()).reversedArray()
+
+    var answer = 0
+    for (i in 0..left.lastIndex) {
+        answer = max(answer, left[i] + right[i] -1)
+    }
+    println(answer)
+
+}
+
+private fun findDp(N: Int, arr: IntArray): IntArray {
+    val dp = IntArray(N)
+    dp[0]=1
+    for (i in 1 until N) {
+        dp[i] = 1
+        for (j in 0 until i) {
+            if (arr[i] > arr[j])
+                dp[i] = max(dp[i], dp[j] + 1)
+        }
+    }
+
+    for (i in 1..dp.lastIndex) {
+        if (dp[i] < dp[i-1])
+            dp[i] = dp[i-1]
+    }
+
+    return dp
+}
