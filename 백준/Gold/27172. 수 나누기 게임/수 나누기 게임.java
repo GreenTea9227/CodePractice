185c372b07fd93ee0e47748c6c3a7c9cd947c270
GreenTea <sjohn9227@naver.com>
2024-03-22 01:38:21 +0900

    [Gold V] Title: 수 나누기 게임, Time: 688 ms, Memory: 56140 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -8,25 +7,28 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static final int MAX_NUM = 1000001;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int MAX_NUM = 1;
 
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] arr = new int[N];
 
-		int[][] full = new int[MAX_NUM][2];
+		
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			full[arr[i]][0] = 1;
+			MAX_NUM = Math.max(arr[i], MAX_NUM);
 		}
 
-		for (int i = 0; i < arr.length; i++) {
-			int num = arr[i];
-			for (int j = num * 2; j < MAX_NUM; j += num) {
+		int[][] full = new int[MAX_NUM+1][2];
+		for (int i : arr) {
+			full[i][0] = 1;
+		}
+
+		for (int num : arr) {
+			for (int j = num * 2; j < full.length; j += num) {
 				if (full[j][0] == 1) {
 					full[j][1] -= 1;
 					full[num][1] += 1;
@@ -34,9 +36,11 @@ public class Main {
 			}
 		}
 
-		for (int i = 0; i < arr.length; i++) {
-			bw.write(full[arr[i]][1] + " ");
+		StringBuilder sb = new StringBuilder();
+		for (int j : arr) {
+			sb.append(full[j][1]).append(" ");
 		}
+		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 
