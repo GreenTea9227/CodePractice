cf61620b43de033364ae0465b4f7f05532039b35
GreenTea <sjohn9227@naver.com>
2024-10-01 23:05:42 +0900

    [Silver III] Title: 두 수의 합, Time: 292 ms, Memory: 25640 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,43 +1,25 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-                list.add(Integer.parseInt(st.nextToken()));
-        }
-        int target = Integer.parseInt(br.readLine());
-        list.sort(Comparator.naturalOrder());
+		boolean[] arr = new boolean[1_000_001];
+		for (int i = 0; i < n; i++) {
+			arr[Integer.parseInt(st.nextToken())] = true;
+		}
+		int target = Integer.parseInt(br.readLine());
+		long count = IntStream.range(0, arr.length)
+							  .filter(i -> target - i <= 1_000_000 && target - i >=0 && arr[i] && arr[target - i])
+							  .count();
 
-        int start = 0;
-        int end = list.size() - 1;
-        int count = 0;
-        while (start < end) {
-            int sum = list.get(start) + list.get(end);
-            if (sum == target) {
-                count++;
-                start++;
-                end--;
-            } else if (sum > target) {
-                end--;
-            } else {
-                start++;
-            }
-        }
-        System.out.println(count);
-
-
-
-    }
+		System.out.println(count / 2);
+	}
 }
