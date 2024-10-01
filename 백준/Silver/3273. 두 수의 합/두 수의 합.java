c6e47686c93aca312cce249b7455fe552a7989ec
GreenTea <sjohn9227@naver.com>
2024-10-01 22:39:30 +0900

    [Silver III] Title: 두 수의 합, Time: 352 ms, Memory: 27668 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,32 +13,31 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] arr = new int[n];
-
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            arr[i]= Integer.parseInt(st.nextToken());
+                list.add(Integer.parseInt(st.nextToken()));
         }
         int target = Integer.parseInt(br.readLine());
+        list.sort(Comparator.naturalOrder());
 
-        Arrays.sort(arr);
-
-        int start=0;
-        int end=arr.length-1;
-        int count=0;
+        int start = 0;
+        int end = list.size() - 1;
+        int count = 0;
         while (start < end) {
-            int current = arr[start]+arr[end];
-            if (current==target) {
+            int sum = list.get(start) + list.get(end);
+            if (sum == target) {
                 count++;
                 start++;
                 end--;
-            } else if (current < target) {
-                start++;
-            } else {
+            } else if (sum > target) {
                 end--;
+            } else {
+                start++;
             }
-
         }
         System.out.println(count);
+
+
 
     }
 }
