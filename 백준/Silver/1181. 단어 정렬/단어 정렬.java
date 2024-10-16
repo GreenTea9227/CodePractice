f3a9008c46bdecc0800e4bea0add0ca23a1f319b
GreenTea <sjohn9227@naver.com>
2024-10-16 22:27:05 +0900

    [Silver V] Title: 단어 정렬, Time: 444 ms, Memory: 22136 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,23 +1,20 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-        Queue<String> queue = new PriorityQueue<>(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()));
-        for (int i = 0; i < T; i++) {
-            String str = br.readLine();
-            if (!queue.contains(str)) {
-                queue.offer(str);
-            }
-        }
-        while (!queue.isEmpty()) {
-            bw.write(queue.poll()+"\n");
-            bw.flush();
-        }
-        bw.close();
-    }
+	public static void main(String[] args) throws IOException {
+		TreeSet<String> set = new TreeSet<>(Comparator.comparing(String::length)
+			.thenComparing(String::compareTo));
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < T; i++) {
+			set.add(br.readLine());
+		}
+
+		while (!set.isEmpty()) {
+			System.out.println(set.pollFirst());
+		}
+	}
 }
