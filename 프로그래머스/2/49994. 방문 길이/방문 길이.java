870c1791dae33cbdd3bdafa938df6f923d9a1739
GreenTea <sjohn9227@naver.com>
2025-03-23 21:31:38 +0900

    [level 2] Title: 방문 길이, Time: 17.37 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -9,10 +9,10 @@ class Solution {
         map.put('R',new int[]{1,0});
         map.put('D',new int[]{0,-1});
         
-        
+        // 경로 등록
         Set<String> set = new HashSet<>();
         
-        
+        // 시작 0,0
         int cx = 0;
         int cy = 0;
         
@@ -25,16 +25,18 @@ class Solution {
                 continue;
             }
             
+            // 통과 시 경로 등록(한 쪽만 등록하면 반대편이 문제가 됨)
             set.add(""+cx+cy+nx+ny);
             set.add(""+nx+ny+cx+cy);
             cx = nx;
             cy = ny;
-            
         }
         
+        // 양방향을 등록하였으므로 2로 나누기
         return set.size()/2;
     }
     
+    // 범위 체크
     private boolean check(int x, int y) {
         return -5 <= x && x <= 5 && -5<=y && y<=5;
     }
