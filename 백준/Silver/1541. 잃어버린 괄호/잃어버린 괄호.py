8b66a642eb1329a735084dd6fa6482383edb32ae
GreenTea <sjohn9227@naver.com>
2025-10-15 22:54:09 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,13 +1,3 @@
-import re
 formula = input()
-
-split = re.split(r'-', formula)
-
-if len(split) == 1:
-    print(sum(map(int,re.split(r'\+', formula))))
-else:
-    ans = sum(map(int, re.split(r'\+', split[0])))
-    for i in range(1, len(split)):
-        ans -= sum(map(int, re.split(r'\+', split[i])))
-    print(ans)
-
+parts = formula.split('-')
+print(sum(map(int, parts[0].split('+'))) - sum(sum(map(int, p.split('+'))) for p in parts[1:]))
